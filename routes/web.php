6c4b534675d21d5f848204a87ae7d481bb8aa949
kazi-aftabur-rahman <aftab267@gmail.com>
2022-09-05 20:48:28 +0600

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\phone;
+
+
+
+
+Route::get('/', function () {
+    $phone=User::find(1);
+    $phone=User::find(2)->phone;
+    //return $phone;
+
+    $user=phone::find(1);
+    $user=phone::find(1)->user;
+    //return $user;
+    $users=User::all();
+    //return $users;
+
+    return view('welcome',compact('users'));
+});
